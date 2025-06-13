3805a52d5ca5b14661f4352bba561d72df439aaf
encyklopedia <149690701+seegyulater@users.noreply.github.com>
2025-06-13 12:43:44 +0800

    [feat] - added animation loading screen

--- newcy-project/src/App.jsx
+++ newcy-project/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { useState } from "react";
 import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
-      <LoadingScreen />
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        
+      </div>
     </>
   );
 }

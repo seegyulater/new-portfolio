da73e8722a936b19b815876b6f5f85ea5b14ee53
encyklopedia <149690701+seegyulater@users.noreply.github.com>
2025-06-15 12:30:48 +0800

    [feat day 2] - bulding responsive navigation bar, added navbar and mobilemenu

--- newcy-project/src/App.jsx
+++ newcy-project/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
@@ -13,7 +17,8 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     </>
   );

6844d3b3903e128f4381dc5ec566436832f84c32
encyklopedia <149690701+seegyulater@users.noreply.github.com>
2025-07-21 02:26:09 +0800

    [FEAT] - Added background ui using galaxy react bits framework and update new dependencies

--- newcy-project/src/App.jsx
+++ newcy-project/src/App.jsx
@@ -9,6 +9,7 @@ import { Projects } from "./components/sections/Projects";
 import SplashCursor from "./components/SplashCursor";
 import { RevealOnScroll } from "./components/RevealOnScroll";
 import { Contact } from "./components/sections/Contact";
+import Galaxy from "./components/Galaxy";
 import "./index.css";
 
 function App() {
@@ -18,13 +19,25 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden">
+        <div className="absolute inset-0">
+          <Galaxy
+            mouseRepulsion={true}
+            mouseInteraction={true}
+            density={1.5}
+            glowIntensity={0.5}
+            saturation={0.8}
+            hueShift={240}
+          />
+        </div>
+      </div>
       <div
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
         <Projects />
